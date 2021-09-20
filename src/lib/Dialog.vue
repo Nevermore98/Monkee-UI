f1d3a4382f9554222f1835d21be18f49214db556
Nevermore98 <nevermorey98@qq.com>
2021-09-20 20:51:49 +0800

    完成 Dialog visible 属性

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,26 +1,108 @@
 <template>
-  <div class="dialog-overlay"></div>
-  <div class="dialog-wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行字</p>
-      <p>第二行字</p>
-    </main>
-    <footer>
-      <Button>确认</Button>
-      <Button>取消</Button>
-    </footer>
-  </div>
+  <template v-if="visible">
+    <div class="monkee-dialog-overlay"></div>
+    <div class="monkee-dialog-wrapper">
+      <div class="monkee-dialog">
+        <header>标题 <span class="monkee-dialog-close"></span></header>
+        <main>
+          <p>第一行字</p>
+          <p>第二行字</p>
+        </main>
+        <footer>
+          <Button type="info">确认</Button>
+          <Button>取消</Button>
+        </footer>
+      </div>
+    </div>
+  </template>
 </template>
 
 <script lang="ts">
 import Button from './Button.vue';
 
 export default {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    }
+  },
   components: {Button}
 };
 </script>
 
 <style lang="scss">
+$radius: 16px;
+$border-color: #d9d9d9;
+.monkee-dialog {
+  background: white;
+  border-radius: $radius;
+  min-width: 20em;
+  max-width: 80%;
 
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    font-weight: bold;
+  }
+
+  > main {
+    padding: 16px 16px;
+  }
+
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
 </style>
