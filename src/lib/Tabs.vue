ab5881109f75df4ae90d6081442ee78b3d648c5f
Nevermore98 <nevermorey98@qq.com>
2021-09-21 16:44:24 +0800

    嵌套渲染 Tabs 内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
-    Tabs
+    <div v-for="(title, index) in titles">{{ title }}</div>
+    <component :is="content" v-for="(content,index) in defaults" :key="index"/>
   </div>
 </template>
 
-<!--<script lang="ts">-->
+<script lang="ts">
+import Tab from './Tab.vue';
 
-
-<!--export default  {-->
-
-<!--}-->
-<!--</script>-->
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab');
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return {
+      defaults,
+      titles
+    };
+  }
+};
+</script>
 
 <!--<style lang="scss" >-->
 
