d3fba192d8b735b3405726e7651517acbeaace82
Nevermore98 <nevermorey98@qq.com>
2021-09-23 23:32:28 +0800

    实现展示源代码功能

---
+++ src/components/SwitchDemo1.vue
@@ -0,0 +1,25 @@
+<demo>
+常规用法
+</demo>
+
+<template>
+  <Switch v-model:value="isOpen" />
+</template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {
+  ref
+} from 'vue'
+export default {
+  components: {
+    Switch,
+  },
+  setup() {
+    const isOpen = ref(false)
+    return {
+      isOpen
+    }
+  }
+}
+</script>
---
+++ src/components/SwitchDemo2.vue
@@ -0,0 +1,24 @@
+<demo>
+支持 disabled
+</demo>
+<template>
+  <Switch v-model:value="isOpen" disabled />
+</template>
+
+<script lang="ts">
+import Switch from '../lib/Switch.vue'
+import {
+  ref
+} from 'vue'
+export default {
+  components: {
+    Switch,
+  },
+  setup() {
+    const isOpen = ref(false)
+    return {
+      isOpen
+    }
+  }
+}
+</script>
