fd62fff3e58f6b5f24ae4bfcca7b27e8c254cdbd
Nevermore98 <nevermorey98@qq.com>
2021-09-27 20:57:11 +0800

    将动态引入改为静态引入

--- src/components/Markdown.vue
+++ src/components/Markdown.vue
@@ -10,20 +10,11 @@ import {
 
 export default {
   props: {
-    path: {
+    content: {
       type: String,
       required: true
     }
   },
-  setup(props) {
-    const content = ref<string>(null);
-    import(props.path).then(result => {
-      content.value = result.default;
-    });
-    return {
-      content
-    };
-  }
 };
 </script>
 <style lang="scss">
--- src/router.ts
+++ src/router.ts
@@ -7,10 +7,13 @@ import TabsDoc from './components/tabs/TabsDoc.vue';
 
 import {h} from 'vue';
 import Markdown from './components/Markdown.vue';
+import intro from './markdown/intro.md';
+import getStarted from './markdown/get-started.md';
+import install from './markdown/install.md';
 
 import {createRouter, createWebHashHistory} from 'vue-router';
 
-const md = (filename: string) => h(Markdown, {path: `../markdown/${filename}.md`, key: filename});
+const md = (string: string) => h(Markdown, {content: string, key: string});
 const history = createWebHashHistory();
 export const router = createRouter({
   history: history,
@@ -20,10 +23,10 @@ export const router = createRouter({
       path: '/doc',
       component: Doc,
       children: [
-        {path: '', redirect:'/doc/intro'},
-        {path: 'intro', component: md('intro')},
-        {path: 'get-started', component: md('get-started')},
-        {path: 'install', component: md('install')},
+        {path: '', redirect: '/doc/intro'},
+        {path: 'intro', component: md(intro)},
+        {path: 'get-started', component: md(getStarted)},
+        {path: 'install', component: md(install)},
         {path: 'switch', component: SwitchDoc},
         {path: 'button', component: ButtonDoc},
         {path: 'dialog', component: DialogDoc},
